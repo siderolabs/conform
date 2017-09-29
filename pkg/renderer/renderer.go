05f2fb85f7d0536f7263e7da5911a3ea363ed821
test <andrew@andrewrynhard.com>
2017-09-28 17:08:49 -0700

    feat(renderer): allow templates to be retrieved from URL

--- pkg/renderer/renderer.go
+++ pkg/renderer/renderer.go
@@ -2,6 +2,9 @@ package renderer
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
@@ -14,7 +17,8 @@ type Renderer struct{}
 // Render renders a template.
 func (r *Renderer) Render(m *metadata.Metadata, s string) (string, error) {
 	var wr bytes.Buffer
-	tmpl, err := template.New("").Funcs(sprig.TxtFuncMap()).Parse(s)
+	funcMap := makeFuncMap(m)
+	tmpl, err := template.New("").Funcs(funcMap).Parse(s)
 	if err != nil {
 		return "", err
 	}
@@ -35,4 +39,38 @@ func RenderTemplate(contents string, metadata *metadata.Metadata) (string, error
 	}
 
 	return rendered, nil
+}
+
+func makeFuncMap(m *metadata.Metadata) template.FuncMap {
+	funcMap := sprig.TxtFuncMap()
+
+	funcMap["fromURL"] = func(url string) (string, error) {
+		resp, err := http.Get(url)
+		if err != nil {
+			return "", err
+		}
+		defer func() {
+			_err := resp.Body.Close()
+			if _err != nil {
+				fmt.Printf("Failed to close the response body: %#v", _err)
+			}
+		}()
+		if resp.StatusCode != http.StatusOK {
+			return "", fmt.Errorf("Failed to download from URL: %d", resp.StatusCode)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		rendered, err := RenderTemplate(string(b), m)
+		if err != nil {
+			return "", err
+		}
+
+		return rendered, nil
+	}
+
+	return funcMap
 }

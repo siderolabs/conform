088e0a76b6e04ca521c6b9e03e66acb03b8dfbe8
Andrew Rynhard <andrew@andrewrynhard.com>
2018-10-05 09:27:44 -0700

    chore(*): output binaries (#72)

--- cmd/root.go
+++ cmd/root.go
@@ -47,7 +47,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.conform.yaml)")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is .conform.yaml)")
 	RootCmd.Flags().BoolVar(&debug, "debug", false, "Debug rendering")
 }
 

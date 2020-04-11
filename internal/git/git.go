8726189af55c5278de18b464273b89fed024aa2a
Andrew Rynhard <andrew@andrewrynhard.com>
2020-04-11 10:27:54 -0700

    chore: update go-git
    This brings in the latest version of go-git.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- internal/git/git.go
+++ internal/git/git.go
@@ -10,11 +10,11 @@ import (
 	"path"
 	"path/filepath"
 
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
-	"gopkg.in/src-d/go-git.v4/plumbing/storer"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
 )
 
 // Git is a helper for git.

621e760b19bfb1ae7b1b2846573608b28a2cadd5
Per Abich <per.abich@unikum.net>
2020-11-12 14:59:16 +0100

    feat: add support for requiring a Jira issue in the header
    This is meant to fix #180
    Signed-off-by: Per Abich <per@abich.com>

---
+++ internal/policy/commit/check_jira.go
@@ -0,0 +1,65 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package commit
+
+import (
+	"regexp"
+
+	"github.com/pkg/errors"
+	"github.com/talos-systems/conform/internal/policy"
+)
+
+// JiraCheck enforces that a Jira issue is mentioned in the header
+type JiraCheck struct {
+	errors []error
+}
+
+// Name returns the name of the check.
+func (j *JiraCheck) Name() string {
+	return "Jira issues"
+}
+
+// Message returns to check message.
+func (j *JiraCheck) Message() string {
+	if len(j.errors) != 0 {
+		return j.errors[0].Error()
+	}
+
+	return "Jira issues are invalid"
+}
+
+// Errors returns any violations of the check.
+func (j *JiraCheck) Errors() []error {
+	return j.errors
+}
+
+// ValidateJiraCheck validates if a Jira issue is mentioned in the header
+func (c Commit) ValidateJiraCheck() policy.Check {
+	check := &JiraCheck{}
+	compile := regexp.MustCompile(`^(\w+)( \w+)?: \[(\w+)-\d+\] .*`)
+
+	if compile.MatchString(c.msg) {
+		submatch := compile.FindStringSubmatch(c.msg)
+		jiraProject := submatch[3]
+
+		if !find(c.Header.Jira.Keys, jiraProject) {
+			check.errors = append(check.errors, errors.Errorf("Jira project %s is not a valid jira project", jiraProject))
+		}
+	} else {
+		check.errors = append(check.errors, errors.Errorf("No Jira issue tag found in %q", c.msg))
+	}
+
+	return check
+}
+
+func find(slice []string, value string) bool {
+	for _, elem := range slice {
+		if elem == value {
+			return true
+		}
+	}
+
+	return false
+}
